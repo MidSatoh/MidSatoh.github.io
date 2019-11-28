fcff6004fa3675cda0a92e99dafb5e0d18f35d04
MidSatoh <mid_st@icloud.com>
2019-11-28 17:16:45 +0900

    add maven contents

--- js/custom_toc.js
+++ js/custom_toc.js
@@ -1,4 +1,4 @@
 if ($('#TableOfContents').length > 0) {
-    document.getElementsByClassName("page")[0].style.margin = '0 16rem 0 17rem'
+    document.getElementsByClassName("page")[0].style.margin = '0 17rem 0 18rem'
     document.getElementsByClassName("page")[0].style.padding = '0 1rem 0 4rem'
 }
